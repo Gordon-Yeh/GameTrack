a9b1ef0454f3d31919c5203af2d9c197dc4cc3a1
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-05-27 13:51:46 -0700

    add initial sql table statements

---
+++ db/tables.sql
@@ -0,0 +1,102 @@
+CREATE TABLE Users(
+   user_id CHAR(32) PRIMARY KEY,
+   username VARCHAR(16) UNIQUE,
+   full_name VARCHAR(32),
+   password VARCHAR(32) NOT NULL,
+   age INTEGER NOT NULL,
+   sex VARCHAR(8) NOT NULL,
+   city VARCHAR(16) NOT NULL,
+   province VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE PostalCode(
+  postal_code CHAR(6),
+  province CHAR(2) NOT NULL,
+  city VARCHAR(60) NOT NULL,
+  PRIMARY KEY(postal_code)
+);
+
+CREATE TABLE Location(
+  location_id CHAR(32),
+  postal_code CHAR(6) NOT NULL,
+  street_address VARCHAR(200) NOT NULL,
+  PRIMARY KEY(location_id),
+  FOREIGN KEY(postal_code) REFERENCES PostalCode(postal_code)
+);
+
+CREATE TABLE LocationBooking(
+  booking_id CHAR(32) PRIMARY KEY,
+  start_time NUMBER(19) NOT NULL,
+  end_time NUMBER(19) NOT NULL,
+  location_id CHAR(32) NOT NULL,
+  FOREIGN KEY(location_id) REFERENCES Location(location_id)
+);
+
+CREATE TABLE SportInfo(
+  description CHAR(512) PRIMARY KEY,
+  meta VARCHAR(512) NOT NULL
+);
+
+CREATE TABLE SportDescription(
+  name CHAR(32) PRIMARY KEY,
+  suggested_number_of_player INTEGER,
+  description CHAR(512) NOT NULL,
+  FOREIGN KEY(description) REFERENCES SportInfo(description) ON DELETE CASCADE
+);
+
+CREATE TABLE Event(
+   event_id CHAR(32) PRIMARY KEY,
+   name VARCHAR(32) UNIQUE NOT NULL,
+   team_size INTEGER,
+   is_a_tournament NUMBER(1) NOT NULL,
+   number_of_teams INTEGER NOT NULL,
+   host_user_id CHAR(32) NOT NULL,
+   booking_id CHAR(32) NOT NULL,
+   sport char(32) NOT NULL,
+   FOREIGN KEY (host_user_id) REFERENCES Users ON DELETE CASCADE,
+   FOREIGN KEY (booking_id) REFERENCES LocationBooking ON DELETE CASCADE,
+   FOREIGN KEY (sport) REFERENCES SportDescription(name) ON DELETE SET NULL
+);
+
+CREATE TABLE Team(
+   event_id CHAR(20),
+   team_number INTEGER PRIMARY KEY,
+   name VARCHAR(32) NOT NULL,
+   curr_size INTEGER NOT NULL,
+   max_size INTEGER NOT NULL,
+   FOREIGN KEY (event_id) REFERENCES Event ON DELETE CASCADE
+);
+
+CREATE TABLE UserJoins(
+  user_id CHAR(32),
+  event_id CHAR(32),
+  team_number INTEGER,
+  PRIMARY KEY(user_id, event_id, team_number),
+  FOREIGN KEY(user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(event_id) REFERENCES Event(event_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Message(
+  message_id CHAR(32),
+  sender_user_id CHAR(32) NOT NULL,
+  receiver_user_id CHAR(32) NOT NULL,
+  content VARCHAR(128) NOT NULL,
+  message_timestamp NUMBER(19) NOT NULL,
+  PRIMARY KEY(message_id),
+  FOREIGN KEY(sender_user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(receiver_user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Invite(
+  host_user_id CHAR(32),
+  guest_user_id CHAR(32),
+  event_id CHAR(32),
+  PRIMARY KEY(host_user_id, guest_user_id, event_id),
+  FOREIGN KEY(host_user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(guest_user_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+  FOREIGN KEY(event_id) REFERENCES Event(event_id) ON DELETE CASCADE
+);
+
+
+
+
