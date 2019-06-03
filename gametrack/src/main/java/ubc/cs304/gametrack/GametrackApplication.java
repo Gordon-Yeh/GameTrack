5e73a7d368b2fb3aeebf948c2a7c18f113bebb02
Ali Mohamed <alihossam_10@live.com>
2019-06-03 14:37:17 -0700

    Working MySQL setup java code

--- gametrack/src/main/java/ubc/cs304/gametrack/GametrackApplication.java
+++ gametrack/src/main/java/ubc/cs304/gametrack/GametrackApplication.java
@@ -1,30 +1,81 @@
 package ubc.cs304.gametrack;
 
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
-public class GametrackApplication {
+public class GametrackApplication implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(GametrackApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(GametrackApplication.class, args);
     }
-
+    
+    public void setupJDBC() {
+    	
+    }
+    
     @Autowired
     JdbcTemplate jdbcTemplate;
 
-//    @Override
-//    public void run(String... strings) throws Exception {
-//
-//        Connection con = DriverManager.getConnection(
-//                "jdbc:oracle:thin:@dbhost.students.cs.ubc.ca:1522:stu", System.getenv("ORA_USERNAME"), System.getenv("ORA_PASSWORD"));
-//
-//
-//    }
+    /* Example code from https://spring.io/guides/gs/relational-data-access/ */
+    @Override
+    public void run(String... strings) throws Exception {
+
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id SERIAL, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Arrays.asList("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long").stream()
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info(String.format("Inserting customer record for %s %s", name[0], name[1])));
+
+        // Uses JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                "SELECT id, first_name, last_name FROM customers WHERE first_name = ?", new Object[] { "Josh" },
+                (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name"))
+        ).forEach(customer -> log.info(customer.toString()));
+
+    }
+    
+    public class Customer {
+        private long id;
+        private String firstName, lastName;
+
+        public Customer(long id, String firstName, String lastName) {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        @Override
+        public String toString() {
+            return String.format(
+                    "Customer[id=%d, firstName='%s', lastName='%s']",
+                    id, firstName, lastName);
+        }
+
+        // getters & setters omitted for brevity
+    }
 }
