196f6dc2a98ea07298633b21fad341c9f0e8a434
Gordon Yeh <gordon.yeh.dev@gmail.com>
2019-06-17 14:24:16 -0700

    add script to set up db with docker

---
+++ docker.sh
@@ -0,0 +1,17 @@
+IMAGE="mysql"
+DB_NAME="GAMETRACK"
+DB_DOCKER_INSTANCE_NAME="gametrack-db-instance"
+ROOT_PW="test"
+
+# build db for gametrack
+docker pull $IMAGE
+
+# set up MySQL DB locally 
+docker container stop $DB_DOCKER_INSTANCE_NAME
+docker rm $DB_DOCKER_INSTANCE_NAME
+docker run \
+  --name $DB_DOCKER_INSTANCE_NAME \
+  -e MYSQL_DATABASE=$DB_NAME \
+  -e MYSQL_ROOT_PASSWORD=$ROOT_PW \
+  -p 3306:3306 \
+  -d $IMAGE
